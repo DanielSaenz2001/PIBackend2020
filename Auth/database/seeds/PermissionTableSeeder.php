1f39595fdcf4f767a057d2377393dad8d563a88f
DanielSaenz2001 <daniel.saenz@upeu.edu.pe>
2020-06-02 00:27:38 -0500

    first commit

---
+++ Auth/database/seeds/PermissionTableSeeder.php
@@ -0,0 +1,60 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Caffeinated\Shinobi\Models\Permission;
+
+class PermissionTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //egresados
+        Permission::create([
+            'name' => 'Navegar Egresados',
+            'slug' => 'users.index',
+            'descripcion' => 'Lista y navega todos los usuarios del sistema',
+        ]);
+
+        Permission::create([
+            'name' => 'Ver detalles Egresados',
+            'slug' => 'users.show',
+            'descripcion' => 'Ver en detalle los datos de los usuarios del sistema',
+        ]);
+        Permission::create([
+            'name' => 'Editar Egresados',
+            'slug' => 'users.index',
+            'descripcion' => 'Editar los usuarios del sistema',
+        ]);
+        Permission::create([
+            'name' => 'Navegar Egresados',
+            'slug' => 'users.destroy',
+            'descripcion' => 'Eliminar los usuarios del sistema',
+        ]);
+        // roles
+        Permission::create([
+            'name' => 'Navegar roles',
+            'slug' => 'users.index',
+            'descripcion' => 'Lista y navega todos los usuarios del sistema',
+        ]);
+
+        Permission::create([
+            'name' => 'Ver detalles roles',
+            'slug' => 'users.show',
+            'descripcion' => 'Ver en detalle los datos de los usuarios del sistema',
+        ]);
+        Permission::create([
+            'name' => 'Editar roles',
+            'slug' => 'users.index',
+            'descripcion' => 'Editar los usuarios del sistema',
+        ]);
+        Permission::create([
+            'name' => 'Navegar roles',
+            'slug' => 'users.destroy',
+            'descripcion' => 'Eliminar los usuarios del sistema',
+        ]);
+    }
+}
